592398130d3d79759253d1ebdb19ac57143e96b7
setrar <setrar@valiha.com>
2023-02-10 18:39:30 -0500

    .

--- 0.keys/.scripts/participation.sh
+++ 0.keys/.scripts/participation.sh
@@ -30,8 +30,8 @@ echo "| :x:                | Projet inexistant             |"
 echo ""
 echo "## :a: Présence"
 echo ""
-echo "|:hash:| Boréal :id:                | Interne            | ssh | Version |"
-echo "|------|----------------------------|--------------------|-----|---------|"
+echo "|:hash:| Boréal :id:                | Interne            | ssh | Version | Proliant GL |"
+echo "|------|----------------------------|--------------------|-----|---------|-------------|"
 
 i=0
 
@@ -41,15 +41,24 @@ do
         -o StrictHostKeyChecking=no \
         -o PasswordAuthentication=no \
         -o ConnectTimeout=5 ${SERVERS[${i}]} lsb_release -a 2>/dev/null`
+   CHASSIS=`ssh -i ~/.ssh/b300098957@ramena.pk \
+        -o StrictHostKeyChecking=no \
+        -o PasswordAuthentication=no \
+        -o ConnectTimeout=5 ${SERVERS[${i}]} ip link | grep enp | wc -l 2>/dev/null`
     # echo "ssh -i ~/.ssh/b300098957@ramena.pk \
     #    -o StrictHostKeyChecking=no \
     #    -o PasswordAuthentication=no \
     #    -o ConnectTimeout=5 ${SERVERS[${i}]} lsb_release -a 2>/dev/null"
     # echo $VERSION
-   OK="| ${i} | ${id} - <image src='https://avatars.githubusercontent.com/u/${AVATARS[$i]}?s=460&v=4' width=20 height=20></image> | \`ssh ${SERVERS[$i]}\` | :heavy_check_mark: | ${VERSION} | "
-   KO="| ${i} | ${id} - <image src='https://avatars.githubusercontent.com/u/${AVATARS[$i]}?s=460&v=4' width=20 height=20></image> | \`ssh ${SERVERS[$i]}\` | :x: | :x: | "
+   OK_G7="| ${i} | ${id} - <image src='https://avatars.githubusercontent.com/u/${AVATARS[$i]}?s=460&v=4' width=20 height=20></image> | \`ssh ${SERVERS[$i]}\` | :heavy_check_mark: | ${VERSION} | G:seven: |"
+   OK_G6="| ${i} | ${id} - <image src='https://avatars.githubusercontent.com/u/${AVATARS[$i]}?s=460&v=4' width=20 height=20></image> | \`ssh ${SERVERS[$i]}\` | :heavy_check_mark: | ${VERSION} | G:six: |"
+   KO="| ${i} | ${id} - <image src='https://avatars.githubusercontent.com/u/${AVATARS[$i]}?s=460&v=4' width=20 height=20></image> | \`ssh ${SERVERS[$i]}\` | :x: | :x: | :x: |"
    if [[ $VERSION == *"Ubuntu"* ]]; then
-       echo ${OK}
+      if [[ $CHASSIS == 4 ]]; then
+          echo ${OK_G7}
+      else
+          echo ${OK_G6}
+      fi
    else
        echo ${KO}
    fi
